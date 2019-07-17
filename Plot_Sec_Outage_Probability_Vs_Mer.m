dedbc3e39f61817ec170adcff2ea1fcdf1b85a64
yash9919 <52064558+yash9919@users.noreply.github.com>
2019-07-17 11:58:00 +0530

    Delete Plot_Sec_Outage_Probability_Vs_Mer.m

--- Plot_Sec_Outage_Probability_Vs_Mer.m
+++
@@ -1,7 +0,0 @@
-load Sec_Outage_Probability_Vs_Mer.mat
-semilogy(lambdas,secrecy_outage_probability(1:7));
-hold on
-semilogy(lambdas,secrecy_outage_probability(8:14));
-hold on
-semilogy(lambdas,secrecy_outage_probability(15:21));
-hold off
